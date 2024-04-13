5d864029b3162638efa973fb0ef2a7dd9a189a40
Guilherme Garcia <garciaguig@gmail.com>
2024-04-13 20:40:20 -0300

    chore: Add configurações adicionais para o server - morgan, helmet e cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import cors from 'cors';
 import * as env from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import routes from './routes';
 
 env.config();
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
