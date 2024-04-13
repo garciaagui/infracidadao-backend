691a1dff01faa8a9f215ae0cf667df3d4a416603
Guilherme Garcia <garciaguig@gmail.com>
2024-04-13 20:24:23 -0300

    feat: Implementa a lógica das rotas em app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import * as env from 'dotenv';
 import express from 'express';
+import 'express-async-errors';
+import routes from './routes';
 
 env.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 export default app;
