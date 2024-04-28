881feb82710e9dffccbb77c93dc92b9d107515e7
Guilherme Garcia <garciaguig@gmail.com>
2024-04-28 10:58:52 -0300

    refactor: Altera script de testes de integração

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -7,7 +7,6 @@ docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 "$DIR/wait-for-it.sh" "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
-ts-node --transpile-only prisma/helpers/seed.ts
 if [ "$#" -eq 0 ]; then
     vitest -c "./vitest.config.integration.ts"
 else
