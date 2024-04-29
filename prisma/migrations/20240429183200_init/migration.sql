f2c5e986ab9eff9206f1f31494065cc043ed77ce
Guilherme Garcia <garciaguig@gmail.com>
2024-04-29 15:34:34 -0300

    refactor: Atualiza as migrations do Prisma

--- prisma/migrations/20240429183200_init/migration.sql
+++ prisma/migrations/20240429183200_init/migration.sql
@@ -1,12 +1,30 @@
 -- CreateEnum
+CREATE TYPE "Role" AS ENUM ('user', 'employee', 'admin');
+
+-- CreateEnum
 CREATE TYPE "Status" AS ENUM ('Aberto', 'Finalizado', 'Andamento');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "occurrences" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(127) NOT NULL,
     "description" VARCHAR(255) NOT NULL,
-    "location" VARCHAR(255) NOT NULL,
+    "neighborhood" VARCHAR(255) NOT NULL,
+    "street" VARCHAR(255) NOT NULL,
+    "reference" VARCHAR(255) DEFAULT '',
     "status" "Status" NOT NULL,
     "image" VARCHAR(255) NOT NULL,
     "user_id" INTEGER NOT NULL,
@@ -15,6 +33,9 @@ CREATE TABLE "occurrences" (
 
     CONSTRAINT "occurrences_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "occurrences" ADD CONSTRAINT "occurrences_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
