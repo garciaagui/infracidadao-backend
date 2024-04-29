80b078710b1b0efb887f95674dd8a2f7964cd639
Guilherme Garcia <garciaguig@gmail.com>
2024-04-29 09:56:27 -0300

    refactor: Altera migration init para add novos campos e organizar a tabela

--- prisma/migrations/20240429125516_init/migration.sql
+++ prisma/migrations/20240429125516_init/migration.sql
@@ -1,9 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('user', 'employee', 'admin');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "email" TEXT NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
