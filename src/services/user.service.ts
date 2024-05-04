e0834bec84eea9e41a51771e44f2939e44a209a0
Guilherme Garcia <garciaguig@gmail.com>
2024-05-04 14:55:21 -0300

    feat: Implementa método privado 'findByEmail' para 'UserService'

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import { PrismaClient, User } from '@prisma/client';
-//
+import * as e from '../exceptions';
+
 export default class UserService {
   private model: PrismaClient;
 
@@ -11,5 +12,15 @@ export default class UserService {
     const users = await this.model.user.findMany({});
 
     return users;
+  }
+
+  private async findByEmail(email: string): Promise<User> {
+    const user = await this.model.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new e.NotFoundException('Não há usuário com esse e-mail');
+    }
+
+    return user;
   }
 }
