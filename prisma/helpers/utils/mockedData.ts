00590aeebdc5049b7753fbb1a7c06b8b922c4e0c
Guilherme Garcia <garciaguig@gmail.com>
2024-05-05 17:36:13 -0300

    fix: Atualiza mockedData do Prisma - Inserir o id manualmente ocasionava bug

--- prisma/helpers/utils/mockedData.ts
+++ prisma/helpers/utils/mockedData.ts
@@ -2,7 +2,6 @@ import { Role, Status } from '@prisma/client';
 
 const users = [
   {
-    id: 1,
     name: 'João Silva',
     email: 'joao@example.com',
     password: '$2a$12$Y5A6LfbOspKyOp4yCFvAUutzoynEHUkS0neXCObTl8SOTt5TtXxwe',
@@ -10,7 +9,6 @@ const users = [
     role: Role.admin
   },
   {
-    id: 2,
     name: 'Maria Oliveira',
     email: 'maria@example.com',
     password: '$2a$12$urjFcjIR5nPJ1Y/MYaIqXu1p9Y//VbhuQKmugxuFL8Hm1dzdHZhn6',
@@ -18,7 +16,6 @@ const users = [
     role: Role.employee
   },
   {
-    id: 3,
     name: 'Carlos Santos',
     email: 'carlos@example.com',
     password: '$2a$12$KYs9rCDu/eggg9BIRKz9Ke0a6gthYJTIdbbwiveFT8SewrYzN42YW',
@@ -29,7 +26,6 @@ const users = [
 
 const occurrences = [
   {
-    id: 1,
     title: 'Vazamento de água na rua principal',
     description:
       'Há um vazamento de água na calçada em frente ao supermercado.',
@@ -41,7 +37,6 @@ const occurrences = [
     userId: 1
   },
   {
-    id: 2,
     title: 'Lâmpada queimada no parque',
     description:
       'Uma das lâmpadas do poste no parque está queimada, deixando a área escura à noite.',
@@ -53,7 +48,6 @@ const occurrences = [
     userId: 2
   },
   {
-    id: 3,
     title: 'Bueiro entupido na esquina',
     description:
       'O bueiro na esquina da Rua das Flores está entupido e causando alagamentos.',
@@ -64,7 +58,6 @@ const occurrences = [
     userId: 1
   },
   {
-    id: 4,
     title: 'Passeio com buracos na Avenida Central',
     description:
       'Os buracos no passeio estão representando um perigo para os pedestres.',
@@ -76,7 +69,6 @@ const occurrences = [
     userId: 3
   },
   {
-    id: 5,
     title: 'Falta de coleta de lixo na Vila dos Pássaros',
     description:
       'O caminhão de lixo não passa pela Vila dos Pássaros há uma semana.',
