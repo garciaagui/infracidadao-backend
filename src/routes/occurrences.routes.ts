53909c0bb2ec346f1ab005006b1446bfa21a6a80
Guilherme Garcia <garciaguig@gmail.com>
2024-05-05 17:51:27 -0300

    feat: Implementa a rota de criação de occurrences

--- src/routes/occurrences.routes.ts
+++ src/routes/occurrences.routes.ts
@@ -4,6 +4,7 @@ import OccurrenceController from '../controllers/occurrence.controller';
 const routers = Router();
 const controller = new OccurrenceController();
 
+routers.post('/', (res, req, next) => controller.create(res, req, next));
 routers.get('/', (res, req, next) => controller.findAll(res, req, next));
 
 export default routers;
