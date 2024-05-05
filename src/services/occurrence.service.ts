42df3fe355c0df627d0048a44806f37ec8f02d84
Guilherme Garcia <garciaguig@gmail.com>
2024-05-05 17:50:46 -0300

    feat: Implementa o método 'create' em 'OccurrenceService'

--- src/services/occurrence.service.ts
+++ src/services/occurrence.service.ts
@@ -1,10 +1,20 @@
 import { Occurrence, PrismaClient } from '@prisma/client';
+import { validateOccurrence } from '../validations';
 
 export default class OccurrenceService {
   private model: PrismaClient;
 
   constructor(model: PrismaClient) {
     this.model = model;
+  }
+
+  public async create(data: Occurrence): Promise<Occurrence> {
+    console.log(data);
+    validateOccurrence(data);
+
+    const created = await this.model.occurrence.create({ data: { ...data } });
+
+    return created;
   }
 
   public async findAll(): Promise<Occurrence[]> {
