90d4952484f21771abf1418e168ce3f96db1e5fc
Guilherme Garcia <garciaguig@gmail.com>
2024-05-07 21:54:27 -0300

    chore: Configura Service e Controller de Occurence para utilizar S3 da AWS

--- src/services/occurrence.service.ts
+++ src/services/occurrence.service.ts
@@ -1,15 +1,17 @@
 import { Occurrence, PrismaClient } from '@prisma/client';
+import { S3 } from 'aws-sdk';
 import { validateOccurrence } from '../validations';
 
 export default class OccurrenceService {
   private model: PrismaClient;
+  private s3: S3;
 
-  constructor(model: PrismaClient) {
+  constructor(model: PrismaClient, s3: S3) {
     this.model = model;
+    this.s3 = s3;
   }
 
   public async create(data: Occurrence): Promise<Occurrence> {
-    console.log(data);
     validateOccurrence(data);
 
     const created = await this.model.occurrence.create({ data: { ...data } });
