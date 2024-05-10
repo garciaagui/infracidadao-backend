fa660b55a032c895610385aab56d34277533f9f5
Guilherme Garcia <garciaguig@gmail.com>
2024-05-09 22:35:05 -0300

    feat: Add middleware do multer na rota de criação de occurrences

--- src/routes/occurrences.routes.ts
+++ src/routes/occurrences.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 import OccurrenceController from '../controllers/occurrence.controller';
 
 const routers = Router();
 const controller = new OccurrenceController();
 
-routers.post('/', (res, req, next) => controller.create(res, req, next));
+const multerMiddleware = multer();
+
+routers.post('/', multerMiddleware.any(), (res, req, next) =>
+  controller.create(res, req, next)
+);
+
 routers.get('/', (res, req, next) => controller.findAll(res, req, next));
 
 export default routers;
