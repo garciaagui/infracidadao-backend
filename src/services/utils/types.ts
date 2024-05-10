fdd3929eb45090abf03e3dd82da6aec5a55df63d
Guilherme Garcia <garciaguig@gmail.com>
2024-05-09 22:33:38 -0300

    feat: Implementa o método 'create' COMPLETO em 'OccurrenceService'

---
+++ src/services/utils/types.ts
@@ -0,0 +1,8 @@
+export type OccurrenceCreationType = {
+  title: string;
+  description: string;
+  neighborhood: string;
+  street: string;
+  reference?: string | null | undefined;
+  userId: number;
+};
