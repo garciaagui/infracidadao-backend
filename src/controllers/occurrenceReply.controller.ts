1ce98606042af066e9aeb111c4024d712aad8a36
Guilherme Garcia <garciaguig@gmail.com>
2024-05-16 15:42:49 -0300

    feat: Implementa método 'create' em OccurrenceReplyController

--- src/controllers/occurrenceReply.controller.ts
+++ src/controllers/occurrenceReply.controller.ts
@@ -1,3 +1,5 @@
+import { NextFunction, Request, Response } from 'express';
+import awsS3 from '../libs/awsS3';
 import prisma from '../libs/prisma';
 import OccurrenceReplyService from '../services/occurrenceReply.service';
 
@@ -5,6 +7,20 @@ export default class OccurrenceReplyController {
   private service: OccurrenceReplyService;
 
   constructor() {
-    this.service = new OccurrenceReplyService(prisma);
+    this.service = new OccurrenceReplyService(prisma, awsS3);
+  }
+
+  public async create(req: Request, res: Response, next: NextFunction) {
+    const textData = req.body;
+    const fileData = req.files as Express.Multer.File[];
+
+    try {
+      const created = await this.service.create(textData, fileData);
+      return res
+        .status(201)
+        .json({ message: 'Resposta criada!', occurrence: created });
+    } catch (error) {
+      next(error);
+    }
   }
 }
