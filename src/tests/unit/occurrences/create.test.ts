2e3c2a45811a876d6b4a6e856551d60aa5c8a378
Guilherme Garcia <garciaguig@gmail.com>
2024-05-29 14:57:16 -0300

    refactor: Remove comentários no arquivo de testes unitários de create de occurrences

--- src/tests/unit/occurrences/create.test.ts
+++ src/tests/unit/occurrences/create.test.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { BadRequestException } from '../../../exceptions';
 import OccurrenceService from '../../../services/occurrence.service';
